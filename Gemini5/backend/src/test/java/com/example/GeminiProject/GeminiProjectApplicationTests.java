ff4042424ca5a5e04b5af1757cedb1dcd009bdff
SHERATONS <act22970@gmail.com>
2025-04-07 15:07:07 +0700

    add backend

---
+++ Gemini5/backend/src/test/java/com/example/GeminiProject/GeminiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GeminiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
