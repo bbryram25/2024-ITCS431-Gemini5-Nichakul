c3a7ff4b40c0a9a73fb1e28c2dc7d526461f0402
bbryram25 <nichakul.kon@student.mahidol.ac.th>
2025-04-23 19:27:45 +0700

    update SciencePlanStatus.java

--- Gemini5/backend/GeminiProject/src/main/java/com/example/GeminiProject/Enum/SciencePlanStatus.java
+++ Gemini5/backend/GeminiProject/src/main/java/com/example/GeminiProject/Enum/SciencePlanStatus.java
@@ -1,5 +1,12 @@
 package com.example.GeminiProject.Enum;
 
 public enum SciencePlanStatus {
-    
+    CREATED,
+    TESTED,
+    SUBMITTED,
+    VALIDATED,
+    RUNNING,
+    INVALIDATED,
+    COMPLETE
 }
+
