30ff4e3e3250cc60c94c0edb99d0bd063f08ceda
bbryram25 <nichakul.kon@student.mahidol.ac.th>
2025-04-23 21:12:17 +0700

    update SciencePlanRepository.java

--- Gemini5/backend/GeminiProject/src/main/java/com/example/GeminiProject/Repository/SciencePlanRepository.java
+++ Gemini5/backend/GeminiProject/src/main/java/com/example/GeminiProject/Repository/SciencePlanRepository.java
@@ -1,5 +1,10 @@
 package com.example.GeminiProject.Repository;
 
-public class SciencePlanRepository {
-    
+import com.example.GeminiProject.Model.SciencePlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SciencePlanRepository extends JpaRepository<SciencePlan, String> {
+    // You can define custom query methods here if needed
 }
